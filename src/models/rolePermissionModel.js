4e19983c38b4d2f81957bed1b0b717602a13709c
esmanurgokkaya <esmanurgkky10@gmail.com>
2025-07-17 10:37:01 +0300

    update role permission  model

--- src/models/rolePermissionModel.js
+++ src/models/rolePermissionModel.js
@@ -1,20 +1,34 @@
-const db = require("../config/db");
+const db = require("../config/db"); 
 
-// Role'a permission ekle
+/**
+ * @desc Belirtilen role, belirtilen permission'ı ekler
+ * @param {number} roleId - Rol ID'si
+ * @param {number} permissionId - Eklenmek istenen yetki ID'si
+ * @returns {Promise<Object>} - Ekleme işleminin sonucu
+ */
 const addPermissionToRole = async (roleId, permissionId) => {
   const query = `INSERT INTO role_permissions (role_id, permission_id) VALUES (?, ?)`;
   const [result] = await db.execute(query, [roleId, permissionId]);
   return result;
 };
 
-// Role'dan permission kaldır
+/**
+ * @desc Belirtilen role ait bir permission'ı kaldırır
+ * @param {number} roleId - Rol ID'si
+ * @param {number} permissionId - Kaldırılacak yetki ID'si
+ * @returns {Promise<Object>} - Silme işleminin sonucu
+ */
 const removePermissionFromRole = async (roleId, permissionId) => {
   const query = `DELETE FROM role_permissions WHERE role_id = ? AND permission_id = ?`;
   const [result] = await db.execute(query, [roleId, permissionId]);
   return result;
 };
 
-// Role'un tüm permissionlarını getir
+/**
+ * @desc Belirli bir role ait tüm permissionları getirir
+ * @param {number} roleId - Rol ID'si
+ * @returns {Promise<Array>} - Role ait yetki listesi
+ */
 const getPermissionsByRoleId = async (roleId) => {
   const query = `
     SELECT p.*
@@ -26,7 +40,11 @@ const getPermissionsByRoleId = async (roleId) => {
   return rows;
 };
 
-// Bir permission'a sahip roller (isteğe bağlı)
+/**
+ * @desc Belirli bir permission'a sahip tüm rolleri getirir
+ * @param {number} permissionId - Permission ID'si
+ * @returns {Promise<Array>} - Permission'a sahip roller
+ */
 const getRolesByPermissionId = async (permissionId) => {
   const query = `
     SELECT r.*
