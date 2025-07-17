ebc7a67ca3c3c16f1a1dfd2702f34a1782d30c54
esmanurgokkaya <esmanurgkky10@gmail.com>
2025-07-17 10:22:59 +0300

    update permission controller

--- src/controller/permissionController.js
+++ src/controller/permissionController.js
@@ -1,6 +1,42 @@
 const {getAllPermissions, createPermission, updatePermissionById, deletePermissionById} = require("../models/permissionModel");
 const logger = require("../utils/logger");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Permissions
+ *   description: Permission management
+ */
+
+
+/**
+ * @swagger
+ * /permissions:
+ *   get:
+ *     summary: Get all permissions
+ *     tags: [Permissions]
+ *     responses:
+ *       200:
+ *         description: List of all permissions
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 permissions:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: view_users
+ *       500:
+ *         description: Server error while fetching permissions
+ */
 
 const listPermissions= async (req, res) => {
     try{
@@ -17,6 +53,47 @@ const listPermissions= async (req, res) => {
 
 
 
+/**
+ * @swagger
+ * /permissions:
+ *   post:
+ *     summary: Create a new permission
+ *     tags: [Permissions]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: edit_users
+ *     responses:
+ *       201:
+ *         description: Permission successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Permission successfully created
+ *                 permission:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 2
+ *                     name:
+ *                       type: string
+ *                       example: edit_users
+ *       500:
+ *         description: Server error while creating permission
+ */
 const createPermissions = async(req,res) => {
     const {name} = req.body;
     try{
@@ -31,7 +108,47 @@ const createPermissions = async(req,res) => {
     }
 };
 
-
+/**
+ * @swagger
+ * /permissions/{id}:
+ *   put:
+ *     summary: Update a permission by ID
+ *     tags: [Permissions]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID of the permission to update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: manage_roles
+ *     responses:
+ *       200:
+ *         description: Permission successfully updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Permission successfully updated
+ *       404:
+ *         description: Permission not found
+ *       500:
+ *         description: Server error while updating permission
+ */
 const updatePermissions = async (req, res) => {
     const {id} = req.params;
     const {name} = req.body;
@@ -48,6 +165,35 @@ const updatePermissions = async (req, res) => {
     }
 };
 
+/**
+ * @swagger
+ * /permissions/{id}:
+ *   delete:
+ *     summary: Delete a permission by ID
+ *     tags: [Permissions]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID of the permission to delete
+ *     responses:
+ *       200:
+ *         description: Permission successfully deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Permission successfully deleted
+ *       404:
+ *         description: Permission not found
+ *       500:
+ *         description: Server error while deleting permission
+ */
 const deletePermissions = async (req, res) => {
     const {id} = req.params;
     try {
